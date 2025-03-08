cde80d0ba8bfb2968a3f3c9d8577ab34dd3128f7
Ayush Singh <ayushtech8085@gmail.com>
2025-03-08 16:51:33 +0530

    Add error handling to RQSuperHeroesPage and SuperHerosPage for improved data fetching

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,10 +6,17 @@ const fetchRQSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", fetchRQSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchRQSuperHeroes
+  );
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
