6e25636f01d3e080f487379b6fefe7a59d3289f8
Ayush Singh <ayushtech8085@gmail.com>
2025-03-09 15:22:18 +0530

    (Optimistic Updates)  Add new superheroes in useAddSuperHeroData hook and so the expected data before the api calls for the enhance user experience

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -24,15 +24,39 @@ export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
 
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries("super-heroes");   // Query Invalidation
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries("super-heroes");   // Query Invalidation
 
+    //   queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
+
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const prevHeroData = queryClient.getQueryData("super-heroes");
       queryClient.setQueryData("super-heroes", (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+
+      return {
+        prevHeroData, // rollback the data in case of failure
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heroes", context.prevHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes");
     },
   });
 };
